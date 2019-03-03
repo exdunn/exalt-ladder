9802ad2d5c0ef54ef727016bf1096c660f8ea603
exdunn <ahdunn@uci.edu>
2019-03-03 10:25:40 -0800

    import bootstrap, add char_label, ladder, navbar components

---
+++ src/components/char_label.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class CharLabel extends Component {
+  state = {};
+  render() {
+    return <a />;
+  }
+}
+
+export default CharLabel;
---
+++ src/components/ladder.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Ladder extends Component {
+  state = {};
+  render() {
+    return <A />;
+  }
+}
+
+export default Ladder;
